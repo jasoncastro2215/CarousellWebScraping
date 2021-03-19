fd25ad96ed626b4a6972f593b0d14cbf4088b11d
jason castro <jason.castro@sumofy.me>
2021-03-19 14:53:12 +0800

    align code with the latest update of carousell

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,16 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        String linkCSS = ".D_gC";
+
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
         driver.navigate().to("https://www.carousell.ph/search/bedspace");
         int page = 13;
         int noOfCards;
-        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".D_gu"), 0));
-        List<WebElement> cards = driver.findElements(By.cssSelector(".D_gu"));
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(linkCSS), 0));
+        List<WebElement> cards = driver.findElements(By.cssSelector(linkCSS));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         noOfCards = cards.size();
         int prevNoOfCards = 0;
@@ -41,9 +43,9 @@ public class Main {
             js.executeScript("javascript:window.scrollBy(0, 10000)");
             List<WebElement> loadMoreButton = driver.findElements(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div[2]/main/div/button"));
             loadMoreButton.get(0).click();
-            wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".D_gu"), noOfCards));
+            wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(linkCSS), noOfCards));
             prevNoOfCards = noOfCards;
-            cards = driver.findElements(By.cssSelector(".D_gu"));
+            cards = driver.findElements(By.cssSelector(linkCSS));
             noOfCards = cards.size();
         }
     }
